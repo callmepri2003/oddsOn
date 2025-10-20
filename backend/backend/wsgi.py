35df9fd9884a4265395e7afe8e2ad5f028ca3ea3
callmepri <140776053+callmepri@users.noreply.github.com>
2025-10-20 13:05:57 +1100

    feat: new wsgi

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add outer backend folder to sys.path
+path = '/home/pri20032003/backend'  # replace with your PythonAnywhere username
+if path not in sys.path:
+    sys.path.append(path)
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
